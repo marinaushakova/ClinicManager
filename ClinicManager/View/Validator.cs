a6e4fda2c6c4c3a3bfdf6773520959b663f3212d
Kevin Carlson <kcarlso2@my.westga.edu>
2015-03-01 15:51:02 -0500

    Added the Validator class with the IsPresent method. currently handles textboxes, comboboxes, and dateTimePickers

---
+++ ClinicManager/View/Validator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClinicManager.View
+{
+    /// <summary>
+    /// Verifies the proper value/format/presence of various form controls
+    /// </summary>
+    class Validator
+    {
+        private const string MESSAGE = "Invalid Input";
+
+        /// <summary>
+        /// Checks that the control passed in has a value
+        /// </summary>
+        /// <param name="control">The control to be checked</param>
+        /// <returns>true if the control has a value, false otherwise</returns>
+        public static bool IsPresent(Control control)
+        {
+            if (control.GetType().ToString() == "System.Windows.Forms.TextBox")
+            {
+                TextBox textBox = (TextBox)control;
+                textBox.Text = textBox.Text.Trim();
+                if (textBox.Text == "")
+                {
+                    MessageBox.Show(textBox.Tag.ToString() + " is a required field.", MESSAGE);
+                    textBox.Focus();
+                    return false;
+                }
+            }
+            else if (control.GetType().ToString() == "System.Windows.Forms.ComboBox")
+            {
+                ComboBox comboBox = (ComboBox)control;
+                if (comboBox.SelectedIndex == -1)
+                {
+                    MessageBox.Show(comboBox.Tag.ToString() + " is a required field.", MESSAGE);
+                    comboBox.Focus();
+                    return false;
+                }
+            }
+            else if (control.GetType().ToString() == "System.Windows.DateTimePicker")
+            {
+                DateTimePicker dateTimePicker = (DateTimePicker)control;
+                if (dateTimePicker.Text == "")
+                {
+                    MessageBox.Show(dateTimePicker.Tag.ToString() + " is a required field.", MESSAGE);
+                    dateTimePicker.Focus();
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
