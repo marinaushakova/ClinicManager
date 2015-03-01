d3867372a5db008fa2fe2653834987f3d1dd26ed
jricha22 <jeff.richards@earthlink.net>
2015-03-01 13:23:53 -0500

    Oops, add untracked file

---
+++ ClinicManagerData/DAL/PersonDAL.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClinicManagerData.Model;
+
+namespace ClinicManagerData.DAL
+{
+    /// <summary>
+    /// Data access for person
+    /// </summary>
+    public static class PersonDAL
+    {
+        /// <summary>
+        /// Get summary of persons containing only name, ssn, street address, city and phone
+        /// </summary>
+        /// <returns>List of persons</returns>
+        public static List<Person> GetPersonSummary()
+        {
+            List<Person> retval = new List<Person>();
+            return retval;
+        }
+    }
+}
