0f6daa4b5d3d5ddb8bcd196081d5b087ff51a80d
Marina Ushakova <mushako1@my.westga.edu>
2015-03-02 05:14:41 -0800

    Added comments for CurrentUserInfo class.

--- ClinicManagerData/Model/CurrentUserInfo.cs
+++ ClinicManagerData/Model/CurrentUserInfo.cs
@@ -6,17 +6,26 @@ using System.Threading.Tasks;
 
 namespace ClinicManagerData.Model
 {
+    /// <summary>
+    /// Contains username and admin privelege status of currently logged in user
+    /// </summary>
     public class CurrentUserInfo
     {
         private static string currentLoggedInUsername = null;
         private static bool isUserCurrentAdmin;
 
+        /// <summary>
+        /// Get/set CurrentLoggedInUserName
+        /// </summary>
         public static string CurrentLoggedInUsername
         {
             get;
             set;
         }
 
+        /// <summary>
+        /// Get/set IsCurrentUserAdmin
+        /// </summary>
         public static bool IsCurrentUserAdmin
         {
             get;
